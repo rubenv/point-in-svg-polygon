77b5e27c911362c4b8c7caddee065267cec23b37
Ruben Vermeersch <ruben@rocketeer.be>
2016-02-18 13:39:21 +0100

    Run all tests

--- test/segments.js
+++ test/segments.js
@@ -2,7 +2,7 @@ var assert = require("assert");
 var pointInSvgPolygon = require("..");
 
 describe("Segments", function () {
-    it.only("Splits segments correctly", function () {
+    it("Splits segments correctly", function () {
         var result = pointInSvgPolygon.segments("M1,1 L2000,1 L2000,2000 L1,2000 Z");
         assert.equal(result.length, 4);
     });
