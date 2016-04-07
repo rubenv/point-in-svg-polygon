fe7351bbc066988cfc5d412559d8475a02ffc595
Ruben Vermeersch <ruben@rocketeer.be>
2016-04-07 16:31:45 +0200

    Expand operator-s test case

--- test/operator-s.js
+++ test/operator-s.js
@@ -16,17 +16,78 @@ describe("Operator s", function () {
         for (var i = 0; i < coords.length; i++) {
             var c = segment.coords[i];
             var ce = coords[i];
-            assert.equal(Math.abs(c[0] - ce[0]) < 1e-4, true);
-            assert.equal(Math.abs(c[1] - ce[1]) < 1e-4, true);
+            assert.equal(Math.abs(c[0] - ce[0]) < 1e-4, true, c + " equals " + ce);
+            assert.equal(Math.abs(c[1] - ce[1]) < 1e-4, true, c + " equals " + ce);
         }
     }
 
     it("Segments", function () {
+        assert.equal(segments.length, 15);
         assertSegment(segments[0], "bezier3", [
             [550.701, 629.35],
             [550.701, 629.35],
             [550.351, 646.805],
             [550.701, 647.155],
+        ]);
+        assertSegment(segments[1], "bezier3", [
+            [550.701, 647.155],
+            [551.051, 647.505],
+            [564.790, 647.155],
+            [564.790, 647.155],
+        ]);
+        assertSegment(segments[2], "line", [
+            [564.790, 647.155],
+            [564.790, 663.637],
+        ]);
+        assertSegment(segments[3], "line", [
+            [564.790, 663.637],
+            [581.963, 663.637],
+        ]);
+        assertSegment(segments[4], "line", [
+            [581.963, 663.637],
+            [581.963, 680.194],
+        ]);
+        assertSegment(segments[5], "line", [
+            [581.963, 680.194],
+            [601.307, 680.194],
+        ]);
+        assertSegment(segments[6], "line", [
+            [601.307, 680.194],
+            [601.307, 692.233],
+        ]);
+        assertSegment(segments[7], "line", [
+            [601.307, 692.233],
+            [637.778, 692.233],
+        ]);
+        assertSegment(segments[8], "line", [
+            [637.778, 692.233],
+            [637.778, 710.113],
+        ]);
+        assertSegment(segments[9], "bezier3", [
+            [637.778, 710.113],
+            [637.778, 710.113],
+            [513.749, 733.793],
+            [360.762, 681.453],
+        ]);
+        assertSegment(segments[10], "line", [
+            [360.762, 681.453],
+            [428.700, 619.434],
+        ]);
+        assertSegment(segments[11], "line", [
+            [428.700, 619.434],
+            [435.811, 611.763],
+        ]);
+        assertSegment(segments[12], "line", [
+            [435.811, 611.763],
+            [545.499, 603.973],
+        ]);
+        assertSegment(segments[13], "line", [
+            [545.499, 603.973],
+            [550.502, 606.073],
+        ]);
+        assertSegment(segments[14], "line", [
+            [550.502, 606.073],
+            [550.701, 629.350],
         ]);
     });
 
