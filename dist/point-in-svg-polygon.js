96e07d1d266890e4c1942e5df3de25b9c2ffadeb
Ruben Vermeersch <ruben@rocketeer.be>
2018-11-05 08:24:47 +0100

    Rebuild

--- dist/point-in-svg-polygon.js
+++ dist/point-in-svg-polygon.js
@@ -1,4 +1,4 @@
-require=(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({"point-in-svg-polygon":[function(require,module,exports){
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"point-in-svg-polygon":[function(require,module,exports){
 var bezier3Type = "bezier3";
 var lineType = "line";
 
@@ -265,7 +265,7 @@ function arcToCurve(cp1, rx, ry, angle, large_arc, sweep, cp2, recurse) {
     if (recurse) {
         return splitCurves([m2, m3, m4].concat(res));
     } else {
-        res = [m2, m3, m4].concat(res).join().split(","); // .filter(function (i) { return !!i.length; });
+        res = [m2, m3, m4].concat(res).join().split(",").filter(function (i) { return !!i.length; });
         var newres = [];
         for (var i = 0, ii = res.length; i < ii; i++) {
             newres[i] = i % 2 ? rotate(res[i - 1], res[i], rad)[1] : rotate(res[i], res[i + 1], rad)[0];
